6e5901e1526b6ea3438ab9f4c179e7ec0e6928a8
Karol Będkowski <karol.bedkowski@gmail.com>
2024-11-19 18:10:40 +0100

    gui: fix formatting cels in aw channels list

--- icom_icr6/gui_awchannlist.py
+++ icom_icr6/gui_awchannlist.py
@@ -52,16 +52,12 @@ class AWCRow(gui_chanlist.BaseRow):
         )
 
     def __setitem__(self, idx: int, val: object, /) -> None:  # type: ignore
+        # immutable list
         return
 
     def _from_channel(self, channel: model.Channel | None) -> list[object]:
         # valid channel
-        if (
-            channel
-            and not channel.hide_channel
-            and channel.freq
-            and channel.bank != consts.BANK_NOT_SET
-        ):
+        if channel and not channel.hide_channel and channel.freq:
             data = channel.to_record()
             return [data[col] for col, *_ in self.COLUMNS]
 
@@ -75,9 +71,23 @@ class ChannelsList(gui_chanlist.ChannelsList[AWCRow]):
     def set_data(self, data: ty.Iterable[model.Channel | None]) -> None:
         self.sheet.set_sheet_data(list(map(AWCRow, data)))
         self.sheet.set_all_column_widths()
+        for row in range(len(self.sheet.data)):
+            self.update_row_state(row)
 
     def update_row_state(self, row: int) -> None:
-        pass
+        data_row = self.sheet.data[row]
+        chan = data_row.channel
+
+        self._set_cell_ro(row, "offset", not chan.duplex)
+        self._set_cell_ro(row, "tsql_freq", chan.tone_mode not in (1, 2))
+
+        dtsc = chan.tone_mode in (3, 4)
+        self._set_cell_ro(row, "dtsc", not dtsc)
+        self._set_cell_ro(row, "polarity", not dtsc)
+
+        self._set_cell_ro(row, "bank_pos", chan.bank == consts.BANK_NOT_SET)
+        self._set_cell_ro(row, "canceller", not chan.canceller)
+        self._set_cell_ro(row, "canceller freq", not chan.duplex)
 
     def _configure(self) -> None:
         self.sheet.headers([c[1] for c in self.columns])
@@ -99,6 +109,7 @@ class ChannelsList(gui_chanlist.ChannelsList[AWCRow]):
                     )
                 ).align("right")
             elif isinstance(values, (list, tuple)):
+                # show dict-ed value as string
                 col.align("center")
             else:
                 _LOG.error("unknown column %d: %s", idx, colname)
