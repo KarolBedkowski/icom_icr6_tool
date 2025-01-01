b3eec1a25b928481873b4bd47cfea1a7104a6e7f
Karol Będkowski <karol.bedkowski@gmail.com>
2025-01-01 11:36:57 +0100

    fix: fix sorting/packing - handle hidden channels

--- icom_icr6/model_support.py
+++ icom_icr6/model_support.py
@@ -37,20 +37,20 @@ def sort_channels(
 
 
 def _sort_func_name(chan: model.Channel | None) -> str:
-    return chan.name or "\xff" if chan else "\xff"
+    return chan.name or "\xf0" if chan and not chan.hide_channel else "\xff"
 
 
 def _sort_func_name2(chan: model.Channel | None) -> str:
-    return chan.name if chan else ""
+    return chan.name or "" if chan and not chan.hide_channel else "\xff"
 
 
 def _sort_func_freq(chan: model.Channel | None) -> int:
-    return chan.freq if chan and not chan.hide_channel else 0
+    return chan.freq if chan and not chan.hide_channel else 9_999_999_999
 
 
 def _sort_func_pack(chan: model.Channel | None) -> int:
-    return 0 if chan else 1
+    return 0 if chan and not chan.hide_channel else 1
 
 
 def _sort_func_channel(chan: model.Channel | None) -> int:
-    return chan.number if chan else 1400
+    return chan.number if chan and not chan.hide_channel else 1400
