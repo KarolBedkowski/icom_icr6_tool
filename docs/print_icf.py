34d90602cb5e24a9e3a81bcc2327383c0be45943
Karol Będkowski <karol.bedkowski@gmail.com>
2025-01-12 18:48:37 +0100

    chore: add simple raw memory files differ

--- docs/print_icf.py
+++ docs/print_icf.py
@@ -4,30 +4,65 @@
 # Distributed under terms of the GPLv3 license.
 
 """
-Parse and print content of ic-r6 memory using compiled ksy file.
+Parse and print content of raw ic-r6 memory or compare two raw files
+using compiled ksy file
 """
 import sys
+import icecream
+icecream.install()
 
 import icr6mem
 
 def print_object(obj: object, prefix: object = "") -> None:
     if not hasattr(obj, "__dict__"):
-        print(prefix, repr(obj))
+        print(prefix, "=", repr(obj))
         return
 
-    print(prefix, obj.__class__.__name__, {
-        k:v  for k, v in obj.__dict__.items()
-        if k[0] != "_" and not isinstance(v, list)
-    })
+    for k, v in sorted(obj.__dict__.items()):
+        if k[0] == "_":
+            continue
 
-    for k, v in obj.__dict__.items():
-        if k[0] != "_" and isinstance(v, list):
-            for idx, o in enumerate(v):
-                print_object(o, f"{k}[{idx}]")
+        if isinstance(v, list):
+            for idx, vv in enumerate(v):
+                print_object(vv, f"{prefix}:{k}[{idx}]")
+
+        else:
+            print_object(v, f"{prefix}:{k}")
 
 
-if len(sys.argv) != 2:
-    print("file name required")
-else:
+
+def compare_objects(obj1: object, obj2: object, prefix: str = "") -> None:
+    """Both objects have the same attributes, sizes etc, so this work. """
+    if not hasattr(obj1, "__dict__"):
+        if obj1 != obj2:
+            print(f"{prefix}: {obj1!r}  -->  {obj2!r}")
+
+        return
+
+    for k, v1 in obj1.__dict__.items():
+        if k[0] == "_":
+            continue
+
+        v2 = getattr(obj2, k, None)
+
+        if isinstance(v1, list) and isinstance(v2, list):
+            for idx, (o1, o2) in enumerate(zip(v1, v2, strict=True)):
+                compare_objects(o1, o2, f"{prefix}:{k}[{idx}]")
+
+        else:
+            compare_objects(v1, v2, f"{prefix}:{k}")
+
+
+
+
+if len(sys.argv) == 2:
     mem = icr6mem.Icr6mem.from_file(sys.argv[1])
     print_object(mem)
+
+elif len(sys.argv) == 3:
+    mem1 = icr6mem.Icr6mem.from_file(sys.argv[1])
+    mem2 = icr6mem.Icr6mem.from_file(sys.argv[2])
+    compare_objects(mem1, mem2)
+
+else:
+    print("on or two file names required")
